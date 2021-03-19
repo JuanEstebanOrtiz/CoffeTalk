adbd500e52eb60f92d81a70e48bb7d14101a7593
JuanEstebanOrtiz <44711794+JuanEstebanOrtiz@users.noreply.github.com>
2021-03-19 09:49:05 -0500

    creacion de react native formulario
    react native

--- react_native/App.js
+++ react_native/App.js
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <TextInput
+        style={styles.input}
+      />
+      <TextInput
+        style={styles.input}
+      />
+      <Button title='Ingresar' color='#000055'/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
   },
 });
